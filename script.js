72a91223a7603217a3b6b4e5d8c3c1e2ed5a5865
Adrien Surowiec <adrien.surowiec@protonmail.com>
2023-03-10 12:12:53 +0100

    Create basic layout

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const weatherForm = document.getElementById("weatherForm")
+const answerP = document.getElementById("answerP")
 
 function catchWeather(city) {
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=683ab985d7494deabd4ff0c0fd9208cf&units=metric`)
@@ -9,13 +10,15 @@ function catchWeather(city) {
     })
     .then((res) => {
       if (res.main.feels_like >= 10) {
-        console.log(`It's nice to live in ${city}`)
+        console.log(res)
+        answerP.innerHTML = `It's nice to live in ${city}`
       }
       else if (res.main.feels_like < 10) {
-        console.log(`freezin my ass in ${city}`);
+        console.log(res)
+        answerP.innerHTML = `freezin my ass in ${city}`
       }
     })
-    .catch((err) => {console.log("sorry but this city doesn't exist and we don't have weather stations yet on Jupiter")})
+    .catch((err) => {answerP.innerHTML = "sorry but this city doesn't exist and we don't have weather stations yet on Jupiter"})
 }
 
 weatherForm.addEventListener("submit", (event) => {
