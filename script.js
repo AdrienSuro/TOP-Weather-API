f826975113d937c9a77259a6725732cd6794e709
Adrien Surowiec <adrien.surowiec@protonmail.com>
2023-03-10 07:24:46 +0100

    basic form working

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const weatherForm = document.getElementById("weatherForm")
+
 function catchWeather(city) {
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=683ab985d7494deabd4ff0c0fd9208cf&units=metric`)
     .then((res) => {
@@ -16,4 +18,9 @@ function catchWeather(city) {
     .catch((err) => {console.log("sorry but this city doesn't exist and we don't have weather stations yet on Jupiter")})
 }
 
-catchWeather("Krakow");
+weatherForm.addEventListener("submit", (event) => {
+  let userCity = document.getElementById("city");
+  catchWeather(userCity.value)
+  userCity.value = ""
+  event.preventDefault();
+})
